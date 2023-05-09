586176604de106e3edb997aba20b7c93d1986351
DesignerRapheal <owoyemidamilola@gmail.com>
2023-05-09 15:53:11 -0700

    '8 correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # author - rapheal owoyemi (@DesignerRapheal)
 
-def islower(c):
-    """Function checks for lowercase characters."""
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
+def islower(c):def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
+    
