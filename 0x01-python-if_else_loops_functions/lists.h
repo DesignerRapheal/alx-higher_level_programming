d50a740e5b12f162ba1260c96960ba245f989190
DesignerRapheal <owoyemidamilola@gmail.com>
2023-05-09 15:07:01 -0700

    testing

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Singly linked list
+ * @n: An integer value
+ * @next: A pointer to the next node
+ *
+ * Description: This structure represents a node in a singly linked list.
+ *
+ * Author: DesignerRapheal
+ *
+ */
+typedef struct listint_s
+{
+        int n;
+        struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+listint_t *insert_node(listint_t **head, int number);
+
+#endif /* LISTS_H */
