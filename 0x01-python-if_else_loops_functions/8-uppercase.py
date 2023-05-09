6faec63a811f52c5e68d4cf165665c83e29f1bda
DesignerRapheal <owoyemidamilola@gmail.com>
2023-05-09 15:55:37 -0700

    8 again

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 # author - rapheal owoyemi (@DesignerRapheal)
-
-def islower(c):def uppercase(str):
+def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
         print("{}".format(i), end="")
-    print()
-    
+    print() 
