22032882d1699db768b74744c1c7f1de31938d3b
DesignerRapheal <owoyemidamilola2015@gmail.com>
2023-05-08 18:09:11 -0700

    102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    if a < b:
-        return a + b
-    elif c < 100:
-        return a - b
-    else:
-        return (a * b) - c
-
+    return (98 + a ** b)
+#def magic_calculation(a, b)
