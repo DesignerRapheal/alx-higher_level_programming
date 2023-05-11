2c4376e30837f73f7cd7b54392eafc6d724bcdab
DesignerRapheal <owoyemidamilola@gmail.com>
2023-05-11 12:40:23 -0700

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# author - DesignerRapheal
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    hon = 1
+    han = 2
+    print("{} + {} = {}".format(hon, han, add(hon, han)))
+
