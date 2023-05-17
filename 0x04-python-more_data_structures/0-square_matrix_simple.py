2a4af17fe9e803fed10ed9dfc7295a306803ac0e
DesignerRapheal <owoyemidamilola@gmail.com>
2023-05-17 12:42:59 -0700

    testing

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# author - designerrapheal
+
+def square_matrix_simple(matrix=[]):
+    squared_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        squared_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return squared_matrix
